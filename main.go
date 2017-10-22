afa214b7acce67cd3fc0c19f87262134d0a21dbe
amundra2016 <amundra@saltside.se>
2017-10-22 15:46:33 +0530

    Add logs on server start

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("static"))
 
 	http.Handle("/", fs)
+	for i := 0; i < 3; i++ {
+		logrus.Infof("Logs are generated")
+	}
 	http.ListenAndServe(":8080", nil)
 }
